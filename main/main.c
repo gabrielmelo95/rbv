bb8fc0911d29a3d0798af17e0b32bdb59fa2d8b6
gabriel.benevides <gabriel.benevides@salvus.me>
2025-01-14 21:51:37 -0300

    fft runing with mic data

--- main/main.c
+++ main/main.c
@@ -6,12 +6,22 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/i2s.h"
 #include "driver/gpio.h"
 #include "esp_check.h"
 #include "sdkconfig.h"
+#include "esp_system.h"
+#include "soc/gpio_struct.h"
+#include "driver/gpio.h"
+#include "esp_dsp.h"
+
+const char *TAG = "[MAIN]";
 
 #define EXAMPLE_STD_BCLK_IO1 GPIO_NUM_0 // I2S bit clock io number
 #define EXAMPLE_STD_WS_IO1 GPIO_NUM_1   // I2S word select io number
@@ -20,6 +30,13 @@
 
 #define EXAMPLE_BUFF_SIZE 64
 int16_t sBuffer[EXAMPLE_BUFF_SIZE];
+
+#define N_SAMPLES 1024
+int N = N_SAMPLES;
+// Input test array
+__attribute__((aligned(16))) float x1[N_SAMPLES];
+// Window coefficients
+__attribute__((aligned(16))) float wind[N_SAMPLES];
 
 void i2s_install()
 {
@@ -50,13 +67,20 @@ void i2s_setpin()
 
 static void i2s_example_read_task(void *args)
 {
+    int pos = 0;
+
+    esp_err_t ret;
+    ret = dsps_fft2r_init_fc32(NULL, N >> 1);
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Not possible to initialize FFT. Error = %i", ret);
+        return;
+    }
+    // Generate Hann window
+    dsps_wind_hann_f32(wind, N);
+
     while (1)
     {
-        int rangelimit = 3000;
-        printf("%d", rangelimit * -1);
-        printf(" ");
-        printf("%d", rangelimit);
-        printf(" ");
         size_t bytesIn = 0;
         esp_err_t result = i2s_read(I2S_PORT, &sBuffer, EXAMPLE_BUFF_SIZE, &bytesIn, portMAX_DELAY);
 
@@ -74,9 +98,30 @@ static void i2s_example_read_task(void *args)
 
                 // Average the data reading
                 mean /= samples_read;
+                x1[pos] = mean * wind[pos];
+                pos++;
+                if (pos >= N)
+                {
+                    pos = 0;
+                    // FFT Radix-2
+                    unsigned int start_r2 = dsp_get_cpu_cycle_count();
+                    dsps_fft2r_fc32(x1, N >> 1);
+                    // Bit reverse
+                    dsps_bit_rev2r_fc32(x1, N >> 1);
+                    // Convert one complex vector with length N/2 to one real spectrum vector with length N/2
+                    dsps_cplx2real_fc32(x1, N >> 1);
+                    unsigned int end_r2 = dsp_get_cpu_cycle_count();
 
-                // Print to serial plotter
-                printf("%.2f\r\n", mean);
+                    for (int i = 0; i < N / 2; i++)
+                    {
+                        x1[i] = 10 * log10f((x1[i * 2 + 0] * x1[i * 2 + 0] + x1[i * 2 + 1] * x1[i * 2 + 1] + 0.0000001) / N);
+                    }
+
+                    // Show power spectrum in 64x10 window from -100 to 0 dB from 0..N/4 samples
+                    ESP_LOGW(TAG, "Signal x1");
+                    dsps_view(x1, N / 2, 64, 10, -60, 40, '|');
+                    ESP_LOGI(TAG, "FFT Radix 2 for %i complex points take %i cycles", N / 2, end_r2 - start_r2); /*  */
+                }
             }
         }
     }
